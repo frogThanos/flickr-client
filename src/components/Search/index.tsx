bf80f285c06abd540dc17c0c17858c70c25c086a
frogThanos <thanos@afrogleap.com>
2019-11-16 15:08:55 +0100

    add styling to search component

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -2,18 +2,20 @@ import React, { FC } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import TextField from '@material-ui/core/TextField';
 import { searchActions } from '../../redux/actions';
+import useStyles from './useStyles';
 
 const Search: FC = () => {
+  const classes = useStyles();
   const dispatch = useDispatch();
   const searchText: string = useSelector(({ searchReducer }: any) => searchReducer.searchText);
   const handleOnChange = (event: any) => {
     dispatch(searchActions.setSearchText(event.target.value));
   };
   return (
-    <div>
+    <div className={classes.container}>
       <TextField
         label="Search for Flickr images!"
-        margin="normal"
+        className={classes.textField}
         onChange={handleOnChange}
       />
       <h1>{searchText}</h1>
---
+++ src/components/Search/useStyles.ts
@@ -0,0 +1,18 @@
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      marginTop: 40,
+    },
+    textField: {
+      flex: 1,
+      marginLeft: '10%',
+      marginRight: '10%',
+    },
+  }),
+);
+
+export default useStyles;
