e5d1d224bb0ac8abd5b971cfe23193f5e26df3b2
frogThanos <thanos@afrogleap.com>
2019-11-16 19:23:37 +0100

    check for values

--- src/components/Photos/index.tsx
+++ src/components/Photos/index.tsx
@@ -4,6 +4,7 @@ import { RootStateInterface } from '../../redux/types';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
+import TablePagination from "@material-ui/core/TablePagination";
 import useStyles from './useStyles';
 
 const Photos: FC = () => {
@@ -13,7 +14,7 @@ const Photos: FC = () => {
     <div className={classes.container}>
       <GridList cellHeight="auto" className={classes.gridList} cols={4}>
         {
-          photo && photo.map((item: any) => {
+          photo && photo.length !== 0 && photo.map((item: any) => {
             return (
               <GridListTile key={item.id}>
                 <img src={`https://farm${item.farm}.staticflickr.com/${item.server}/${item.id}_${item.secret}_q.jpg`} alt={item.title} />
@@ -25,6 +26,13 @@ const Photos: FC = () => {
           })
         }
       </GridList>
+      <TablePagination
+        component="nav"
+        page={0}
+        rowsPerPage={10}
+        count={100}
+        onChangePage={() => {}}
+      />
     </div>
   )
 };
--- src/redux/actions/photos-actions/index.ts
+++ src/redux/actions/photos-actions/index.ts
@@ -12,9 +12,11 @@ const getPhotosSuccess = (page: number, pages: number, photo: [], total: string)
 
 const fetchPhotos = () => async (dispatch: any, getState: GetState, { flickrAPIService }: ServiceLocatorInterface) => {
   const { searchText } = getState().searchReducer;
-  const result = await flickrAPIService.search(searchText);
-  const { page, pages, photo, total } = result.photos;
-  dispatch(getPhotosSuccess(page, pages, photo, total));
+  if (searchText) {
+    const result = await flickrAPIService.search(searchText);
+    const { page, pages, photo, total } = result.photos;
+    dispatch(getPhotosSuccess(page, pages, photo, total));
+  }
 };
 
 export default {
